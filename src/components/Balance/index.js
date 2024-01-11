045a552c0797b23aaecf9db08584d3c1b4bf7726
Keventon <kevertonguimaraes19@gmail.com>
2024-01-11 19:25:23 -0300

    adicionado prettier e eslint ao projeto

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,84 +1,78 @@
-import React from 'react'
-import {
-    View,
-    Text,
-    StyleSheet
-} from 'react-native'
-import {MotiView} from 'moti'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { MotiView } from 'moti';
 
-export default function Balance({saldo, gastos}) {
-    return(
-        <MotiView
-            style={styles.container}
-            from={{
-                rotateX: '-100deg',
-                opacity: 0
-            }}
-            animate={{
-                rotateX: '0deg',
-                opacity: 1
-            }}
-            transition={{
-                type:'timing',
-                delay: 300,
-                duration: 900
-            }}
-            >
-            <View style={styles.item}>
-                <Text style={styles.itemTitle}>Saldo</Text>
-                <View style={styles.content}>
-                    <Text style={styles.currancySymbol}>R$</Text>
-                    <Text style={styles.balance}>{saldo}</Text>
-                </View>
-            </View>
+export default function Balance({ saldo, gastos }) {
+  return (
+    <MotiView
+      style={styles.container}
+      from={{
+        rotateX: '-100deg',
+        opacity: 0,
+      }}
+      animate={{
+        rotateX: '0deg',
+        opacity: 1,
+      }}
+      transition={{
+        type: 'timing',
+        delay: 300,
+        duration: 900,
+      }}
+    >
+      <View style={styles.item}>
+        <Text style={styles.itemTitle}>Saldo</Text>
+        <View style={styles.content}>
+          <Text style={styles.currancySymbol}>R$</Text>
+          <Text style={styles.balance}>{saldo}</Text>
+        </View>
+      </View>
 
-            <View style={styles.item}>
-                <Text style={styles.itemTitle}>Gastos</Text>
-                <View style={styles.content}>
-                    <Text style={styles.currancySymbol}>R$</Text>
-                    <Text style={styles.expenses}>{gastos}</Text>
-                </View>
-            </View>
-        </MotiView>
-    )
+      <View style={styles.item}>
+        <Text style={styles.itemTitle}>Gastos</Text>
+        <View style={styles.content}>
+          <Text style={styles.currancySymbol}>R$</Text>
+          <Text style={styles.expenses}>{gastos}</Text>
+        </View>
+      </View>
+    </MotiView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#fff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingStart: 18,
-        paddingEnd: 18,
-        marginTop: -24,
-        marginStart: 16,
-        marginEnd: 16,
-        borderRadius: 16,
-        paddingTop: 22,
-        paddingBottom: 22,
-        zIndex: 99,
-    },
-    content:{
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    currancySymbol:{
-        color: '#dadada',
-        marginRight: 6
-    },
-    balance:{
-        fontSize: 22,
-        color: '#2ecc71'
-    },
-    expenses:{
-        fontSize: 22,
-        color: '#e74c3c'
-    },
-    item: {
-
-    },
-    itemTitle:{
-        fontSize: 20,
-        color: '#dadada',
-    }
-})
+  container: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingStart: 18,
+    paddingEnd: 18,
+    marginTop: -24,
+    marginStart: 16,
+    marginEnd: 16,
+    borderRadius: 16,
+    paddingTop: 22,
+    paddingBottom: 22,
+    zIndex: 99,
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  currancySymbol: {
+    color: '#dadada',
+    marginRight: 6,
+  },
+  balance: {
+    fontSize: 22,
+    color: '#2ecc71',
+  },
+  expenses: {
+    fontSize: 22,
+    color: '#e74c3c',
+  },
+  item: {},
+  itemTitle: {
+    fontSize: 20,
+    color: '#dadada',
+  },
+});
--- src/components/Movements/index.js
+++ src/components/Movements/index.js
@@ -1,93 +1,91 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import {MotiView, MotiText, AnimatePresence} from 'moti'
+import { MotiView, MotiText, AnimatePresence } from 'moti';
 
-export default function Movements({data}) {
+export default function Movements({ data }) {
+  const [showValue, setShowValue] = useState(false);
 
-  const [showValue, setShowValue] = useState(false)
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => setShowValue(!showValue)}>
+      <Text style={styles.date}>{data.date}</Text>
+      <View style={styles.content}>
+        <Text style={styles.label}>{data.label}</Text>
 
- return (
-   <TouchableOpacity style={styles.container} onPress={() => setShowValue(!showValue)}>
-    <Text style={styles.date}>{data.date}</Text>
-    <View style={styles.content}>
-      <Text style={styles.label}>{data.label}</Text>
-      
-      {showValue ? (
-        <AnimatePresence exitBeforeEnter>
+        {showValue ? (
+          <AnimatePresence exitBeforeEnter>
             <MotiText
-             style={data.type === 1 ? styles.value : styles.expenses}
-             from={{
-              translateX: 100,
-             }}
-             animate={{
-              translateX: 0,
-             }}
-             transition={{
-              type:"timing",
-              duration: 500
-             }}
-             >
-            {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
+              style={data.type === 1 ? styles.value : styles.expenses}
+              from={{
+                translateX: 100,
+              }}
+              animate={{
+                translateX: 0,
+              }}
+              transition={{
+                type: 'timing',
+                duration: 500,
+              }}
+            >
+              {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
             </MotiText>
-        </AnimatePresence>
-      ) : (
-        <AnimatePresence exitBeforeEnter>
-          <MotiView
-           style={styles.skeleton}
-           from={{
-            opacity: 0
-           }}
-           animate={{
-            opacity: 1
-           }}
-           transition={{
-            type:"timing"
-           }}
-           />
-        </AnimatePresence>
-      )}
-
-    </View>
-   </TouchableOpacity>
+          </AnimatePresence>
+        ) : (
+          <AnimatePresence exitBeforeEnter>
+            <MotiView
+              style={styles.skeleton}
+              from={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{
+                type: 'timing',
+              }}
+            />
+          </AnimatePresence>
+        )}
+      </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     marginBottom: 24,
     borderBottomWidth: 0.5,
-    borderBottomColor: '#dadada'
+    borderBottomColor: '#dadada',
   },
-  date:{
+  date: {
     color: '#dadada',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-  content:{
+  content: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 8,
-    marginBottom: 9
+    marginBottom: 9,
   },
-  label:{
+  label: {
     fontWeight: 'bold',
-    fontSize: 16
+    fontSize: 16,
   },
-   value:{
+  value: {
     fontSize: 16,
     color: '#2ecc71',
-    fontWeight: 'bold'
-   },
-   expenses: {
+    fontWeight: 'bold',
+  },
+  expenses: {
     fontSize: 16,
     color: '#e74c3c',
-    fontWeight: 'bold'
-   },
-   skeleton:{
+    fontWeight: 'bold',
+  },
+  skeleton: {
     marginTop: 6,
     width: 80,
     height: 10,
     backgroundColor: '#dadada',
-    borderRadius: 16
-   }
-})
+    borderRadius: 16,
+  },
+});
